55746c130fb1b32dcb1f6c477d5c82535e31fece
ishitarani07 <itsishita7206@gmail.com>
2026-03-29 13:33:43 +0530

    course added

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,16 +1,19 @@
-import express from 'express';
-import cors from 'cors';
-import routes from './routes/index.js';
-import { notFound, errorHandler } from './middlewares/errorHandler.js';
+import express from "express";
+import cors from "cors";
+import routes from "./routes/index.js";
+import { notFound, errorHandler } from "./middlewares/errorHandler.js";
 
 const app = express();
 
 // CORS
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    origin:
+      process.env.CLIENT_URL ||
+      "http://localhost:5173" ||
+      "https://scalar-assignment-ishita.vercel.app/",
     credentials: true,
-  })
+  }),
 );
 
 // Body parser
@@ -18,7 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // API routes
-app.use('/api/v1', routes);
+app.use("/api/v1", routes);
 
 // Error handling
 app.use(notFound);
